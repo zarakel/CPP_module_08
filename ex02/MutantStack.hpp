24ae03d4b2c71616f5b15ea08ee93e50e4e70524
catrix <kevyn@catrixs-MacBook-Pro.local>
2022-05-13 15:13:46 +0200

    finish this fuck

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,48 @@
+# ifndef MUTANT_STACK_HPP
+# define MUTANT_STACK_HPP
+
+#include <stack>
+#include <iostream>
+#include <deque>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	public:
+		MutantStack<T>( void )
+		{
+			std::cout << "Constuctor Called" << std::endl;
+			return ;
+		}
+		MutantStack<T>( MutantStack const & )
+		{
+			return ;
+		}
+		MutantStack<T> & operator=( MutantStack const & cp)
+		{
+			*this = cp;
+			return (* this );
+		}
+
+		typedef typename std::stack<T>::container_type::iterator iterator; 
+
+		iterator	begin(void)
+		{
+			return (this->c.begin());
+		}
+
+		iterator	end(void)
+		{
+			return (this->c.end());
+		}
+
+		~MutantStack<T>( void )
+		{
+			std::cout << "Destructor Called" << std::endl;
+			return ;	
+		}
+		int b;
+
+};
+
+#endif
